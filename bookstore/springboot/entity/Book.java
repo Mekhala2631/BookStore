243fa061cd13f1fbe5a85769bf452a57547ca90c
Mekhala <mekhalapillutla8@gmail.com>
2024-07-26 19:44:52 +0530

    Add files via upload

---
+++ bookstore/springboot/entity/Book.java
@@ -0,0 +1,85 @@
+package com.bookstore.springboot.entity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "books")
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String title;
+
+    @Column(nullable = false)
+    private String author;
+
+    @Column(nullable = false, unique = true)
+    private String description;
+
+    @Column(nullable = false)
+    private String genre;
+    
+
+	public Book(Long id, String title, String author, String description, String genre) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.author = author;
+		this.description = description;
+		this.genre = genre;
+	}
+
+	public Book() {
+		super();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getGenre() {
+		return genre;
+	}
+
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+    
+}
